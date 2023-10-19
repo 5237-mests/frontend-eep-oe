9013de4fa675f8c98e66815f59c4965c1f937731
5237-mests <msfnmulgeta@gmail.com>
2023-10-20 00:30:30 +0300

    base url changed.

--- src/components/API.js
+++ src/components/API.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
-const con=process.env.REACT_APP_API;
-  console.log(con, '7');
+const con = process.env.REACT_APP_API;
 const API = axios.create({
-    baseURL: 'http://localhost:8000/',
+    baseURL: con,
     withCredentials:true,
 })
 API.defaults.withCredentials = true;
 export default API
+
+
+// baseURL: 'http://localhost:8000/',
